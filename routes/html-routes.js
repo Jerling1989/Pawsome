71e9fb36ac33938fbe273538bdf3309ecb1f9fdb
Jacob <jerling1989@gmail.com>
2017-09-30 16:15:26 -0400

    upload all files

---
+++ routes/html-routes.js
@@ -0,0 +1,17 @@
+
+
+//for testing data insertion of all fields including 
+//images
+
+var path = require("path");
+
+
+module.exports = function(app){
+	app.get("/", function(req, res){
+		res.sendFile(path.join(__dirname, "../public/feed.html"));
+	})
+
+	app.get("/profile", function(req, res){
+		res.sendFile(path.join(__dirname, "../public/profile.html"));
+	})
+};
